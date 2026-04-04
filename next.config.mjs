616a2d2370b570d6d97e0ddda201b6b5eea7e621
Reign <128897265+Reign3418@users.noreply.github.com>
2026-04-03 22:43:56 -0400

    Configure turbopack root to silence warning

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,12 @@ import createNextIntlPlugin from 'next-intl/plugin';
 const withNextIntl = createNextIntlPlugin('./src/i18n.js');
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  experimental: {
+    turbopack: {
+      root: 'e:/UnityBU'
+    }
+  }
+};
 
 export default withNextIntl(nextConfig);
